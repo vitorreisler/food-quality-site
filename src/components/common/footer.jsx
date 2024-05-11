f8d05cc8b24dc967d0850832ee74b36ed41fc49b
vitorreisler <vitorreisler@gmail.com>
2024-05-11 14:55:44 -0300

    teste

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -39,7 +39,7 @@ export function FooterWithSocialLinks() {
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <Typography variant="h5" className="mb-6">
-            Integra Foods
+            Quality Foods
           </Typography>
           <div className="grid grid-cols-3 justify-between gap-4">
             {LINKS.map(({ title, items }) => (
--- src/components/contato.jsx
+++ src/components/contato.jsx
@@ -6,7 +6,7 @@ const Contato = () => {
     return ( 
         <section className="bg-white py-2">
             <form className="flex flex-col flex-wrap gap-3 items-center my-3 " action="">
-                <h1 className="text-3xl">Entre em contato NÃO FINALZIADO</h1>
+                <h1 className="text-3xl">Entre em contato</h1>
                 <InputDefault label={"Nome"}/>
                 <InputDefault label={"Email"}/>
                 <TextareaDefault label={"Mensagem"}  />
